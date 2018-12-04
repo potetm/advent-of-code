7d837d1d6a4f68f33a0ceecd9c16a80199a1a6b3
Tim Pote <pote.timothy@gmail.com>
2018-12-04 12:31:05 -0600

    Finished Day 2, 2018

---
+++ src/common.clj
@@ -0,0 +1,28 @@
+(ns common)
+
+(defn duplicates
+  ([]
+   (fn [rf]
+     (let [seen (volatile! {})]
+       (fn
+         ([] (rf))
+         ([result] (rf result))
+         ([result input]
+          (case (get @seen input)
+            :seen (do (vswap! seen assoc input :emitted)
+                      (rf result input))
+            :emitted result
+            (do (vswap! seen assoc input :seen)
+                result)))))))
+  ([coll]
+   (let [step (fn step [xs seen]
+                (lazy-seq
+                  ((fn [[f :as xs] seen]
+                     (when-let [s (seq xs)]
+                       (case (get seen f)
+                         :seen (cons f (step (rest s)
+                                             (assoc seen f :emitted)))
+                         :emitted (recur (rest s) seen)
+                         (recur (rest s) (assoc seen f :seen)))))
+                    xs seen)))]
+     (step coll {}))))
